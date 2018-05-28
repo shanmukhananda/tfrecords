3f1c8d2976b23dab82277c457b8891d348034a31
Shanmukha <shanmukhananda@gmail.com>
2018-05-28 12:55:01 +0530

    added concurrency for resize script

--- bddtools/resize_images.py
+++ bddtools/resize_images.py
@@ -1,19 +1,28 @@
 from PIL import Image
+import concurrent.futures
 import fnmatch
+import logging
 import logging
 import os
 import sys
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    filename="resize_images.log",
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(processName)s:%(process)d:%(threadName)s:%(thread)d:%(filename)s:%(lineno)d:%(funcName)s:%(message)s"
+)
 
-def main(argv):
+def resize(input_path, output_path):
 
-    assert len(argv) >=3, "usage: resize_images.py image_folder output_folder"
+    logging.debug("resizing {0}".format(input_path))
+    im = Image.open(infile)
+    width, height = im.size
+    size = (int(width / 2), int(height / 2))
+    out = im.resize(size, Image.ANTIALIAS)
+    out.save(outfile, im.format)
 
-    input_dir = argv[1]
-    output_dir = argv[2]
-
+def get_data(input_dir, output_dir):
+    
     assert os.path.isdir(input_dir), ("{0} is not a valid folder".format(input_dir))
 
     if not os.path.exists(output_dir):
@@ -22,14 +31,22 @@ def main(argv):
     jpg_files = fnmatch.filter(os.listdir(input_dir), "*.jpg")
 
     for file in jpg_files:
-        logging.debug("resizing {0}".format(file))
         infile = os.path.join(input_dir, file)
         outfile = os.path.join(output_dir, file)
-        im = Image.open(infile)
-        width, height = im.size
-        size = (width / 2, height / 2)
-        out = im.resize(size, Image.ANTIALIAS)
-        out.save(outfile, "jpeg")
+        yield (infile, outfile)
+
+def main(argv):
+
+    assert len(argv) == 4, "usage: resize_images.py image_folder output_folder num_threads"
+    logging.info("{}".format(argv))
+
+    input_dir = argv[1]
+    output_dir = argv[2]
+    num_threads = int(argv[3])
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for infile, outfile in get_data(input_dir, output_dir):
+            executor.submit(resize, infile, outfile)
 
 if __name__ == "__main__":
     main(sys.argv)
